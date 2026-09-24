9a575e034c75713d749f6fbb856a63c543f5f0c5
Md Mohiuddin <mohiuddinsciencelover-glitch>
2026-09-25 04:31:45 +0600

    Add Google Analytics tracking

--- js/site.js
+++ js/site.js
@@ -1,3 +1,14 @@
+(function loadGoogleAnalytics() {
+  window.dataLayer = window.dataLayer || [];
+  window.gtag = window.gtag || function gtag(){ window.dataLayer.push(arguments); };
+  window.gtag('js', new Date());
+  window.gtag('config', 'G-E433MWD071');
+  const script = document.createElement('script');
+  script.async = true;
+  script.src = 'https://www.googletagmanager.com/gtag/js?id=G-E433MWD071';
+  document.head.appendChild(script);
+})();
+
 document.addEventListener('DOMContentLoaded', () => {
   const menuButton = document.querySelector('.menu-toggle');
   const nav = document.querySelector('.site-nav');
